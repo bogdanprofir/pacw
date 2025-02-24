c8aad930d8c7b07f92217daaca04a0e5b0bb8066
Bogdan <bogdan.profir@yahoo.com>
2025-02-24 09:44:56 +0200

    Update favicon and the rest meta

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,14 @@ import CookieConsent from "./components/CookieConsent";
 export const metadata = {
   title: 'PAC Water',
   description: 'Site oficial - Distribuitor de produse pentru tratarea apei',
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
+    apple: '/apple-touch-icon.png',
+  },
 }
 
 export default function RootLayout({ children }) {
